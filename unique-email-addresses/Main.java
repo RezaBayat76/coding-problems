83d66662553dc4601dd5467803bce69ff5464ebb
reza <reza.bayat76@gmail.com>
2020-04-13 10:26:42 +0430

    add solution

---
+++ unique-email-addresses/Main.java
@@ -0,0 +1,48 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String[] emails = new String[n];
+        for (int i = 0; i < n; i++) {
+            emails[i] = scanner.next();
+        }
+
+        System.out.println(numUniqueEmails(emails));
+    }
+
+    public static int numUniqueEmails(String[] emails) {
+        Set<String> uniqueEmails = new HashSet<>();
+
+        for (String email : emails) {
+            uniqueEmails.add(actualEmail(email));
+        }
+
+        return uniqueEmails.size();
+    }
+
+    public static String actualEmail(String email) {
+        String[] emailSplit = email.split("@");
+
+        StringBuilder sb = new StringBuilder();
+        for (char c : emailSplit[0].toCharArray()) {
+            if (c == '+') {
+                break;
+            }
+
+            if (c == '.') {
+                continue;
+            }
+
+            sb.append(c);
+        }
+
+        sb.append(emailSplit[1]);
+
+        return sb.toString();
+    }
+}
