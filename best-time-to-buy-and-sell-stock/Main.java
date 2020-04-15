aed7df7ad16f732313e01d73d47a79b622c7d689
reza <reza.bayat76@gmail.com>
2020-04-15 17:08:58 +0430

    add solution

---
+++ best-time-to-buy-and-sell-stock/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] prices = new int[n];
+        for (int i = 0; i < n; i++) {
+            prices[i] = scanner.nextInt();
+        }
+
+        System.out.println(maxProfit(prices));
+    }
+
+    public static int maxProfit(int[] prices) {
+        int max = 0;
+        int min = Integer.MAX_VALUE;
+        for (int price : prices) {
+            if (price < min) {
+                min = price;
+            } else if (price - min > max) {
+                max = price - min;
+            }
+        }
+
+        return max;
+    }
+}
